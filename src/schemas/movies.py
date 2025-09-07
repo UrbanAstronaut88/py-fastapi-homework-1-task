f8d0b6867d688ecaefe90d0e84ef86438374cfbb
BondarenkoKirill <bku089@gmail.com>
2025-09-07 17:43:40 +0300

    task done

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,28 @@
-# Write your code here
+from pydantic import BaseModel
+from typing import Optional, List
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: str
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_language: str
+    budget: float
+    revenue: float
+    country: str
+
+    class Config:
+        orm_mode = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
