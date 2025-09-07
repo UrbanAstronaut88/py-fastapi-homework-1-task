40c742cf666aa7212276d41a46c013879b17b6ee
BondarenkoKirill <bku089@gmail.com>
2025-09-07 19:10:05 +0300

    fix next_page and prev_page

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -32,8 +32,8 @@ async def get_movies(
     movies = result.scalars().all()
 
     base_url = "/theater/movies/"
-    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
-    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+    prev_page = f"{base_url}?page={max(page - 1, 1)}&per_page={per_page}"
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}"
 
     return MovieListResponseSchema(
         movies=[MovieDetailResponseSchema.model_validate(m) for m in movies],
